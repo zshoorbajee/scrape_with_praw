16b790540e1d9c7cd59b60e35a63ca41bf2a4e1b
Zaid Shoorbajee <zshoorbajee@gmail.com>
2024-02-23 14:51:53 -0500

    Update combine to use existing combined file as base file. Add option to delete contributing files.

--- combine.py
+++ combine.py
@@ -20,10 +20,20 @@ sub_name = 'washingtondc'
 ## Supported: new
 sort_by = 'new'
 
+## Location of files to combine
 file_dir = './data'
 output_dir = './data/combined'
+
+## Name and path of the combined DataFrame
 name = f'combined_{sort_by}_submissions_{sub_name}_reddit.pkl'
-path = os.path.join(output_dir, name)
+output_path = os.path.join(output_dir, name)
+
+## Whether to delete already merged files
+delete_combined = False
+
+## Creates a "combined" directory if it doesn't already exists
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
 
 ## Grab all filenames matching desired subreddit and sort/filter
 filenames = os.listdir(file_dir)
@@ -31,16 +41,24 @@ filenames = [x for x in filenames if ((sort_by in x) & (sub_name in x))]
 filenames = sorted(filenames)[::-1]
 
 # Start a combined dataframe
-df = pd.read_pickle(os.path.join(file_dir, filenames[0]))
-
-# Save if only one dataframe
-# Otherwise, concat all dataframes, sort with newest at the top and drop duplicates (keeping newest)
-if len(filenames) < 2:
-    df.to_pickle(path)
+if os.path.exists(output_path):
+    df = pd.read_pickle(output_path)
+    print(f'The file {output_path} already exists. Combining newer files with this one.', '\n')
 else:
-    for file in filenames[1:]:
-        _df = pd.read_pickle(os.path.join(file_dir, file))
-        df = pd.concat([df, _df])
-    df.sort_values(by=['datetime', 'submission_age'], ascending=False, inplace=True)
-    df.drop_duplicates(subset=['id'], keep='first', inplace=True)
-    df.to_pickle(path)
+    df = pd.read_pickle(os.path.join(file_dir, filenames[0]))
+    print(f'The file {output_path} does not exist. Starting a new combined file to save to this path.', '\n')
+
+# Concat all dataframes, sort with newest at the top and drop duplicates (keeping newest)
+for file in filenames:
+    filepath = os.path.join(file_dir, file)
+    _df = pd.read_pickle(filepath)
+    df = pd.concat([df, _df])
+    print(f'Added {file} to combined file.')
+    if delete_combined:
+        os.remove(filepath)
+        print(f'Deleted {file}', '\n')
+    else:
+        print('\n')
+df.sort_values(by=['datetime', 'submission_age'], ascending=False, inplace=True)
+df.drop_duplicates(subset=['id'], keep='first', inplace=True)
+df.to_pickle(output_path)
